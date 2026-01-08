99b8c5709590299ed69e76116d58ca1ff6c28ccd
Shiv Shukla <shivshukla00514@gmail.com>
2026-01-09 01:21:15 +0530

    fix: use post instead of get

--- backend/src/routes/auth.routes.ts
+++ backend/src/routes/auth.routes.ts
@@ -3,14 +3,14 @@ import type { Response, Request } from "express";
 
 export const authRouter=Router();
 
-authRouter.get("/signup", (req:Request, res:Response)=>{
+authRouter.post("/signup", (req:Request, res:Response)=>{
   res.status(200).json({msg:"signup endpoint"})
 })
 
-authRouter.get("/signin", (req:Request, res:Response)=>{
+authRouter.post("/signin", (req:Request, res:Response)=>{
   res.status(200).json({msg:"signin endpoint"})
 })
 
-authRouter.get("/logout", (req:Request, res:Response)=>{
+authRouter.post("/logout", (req:Request, res:Response)=>{
   res.status(200).json({msg:"logout endpoint"})
 })
