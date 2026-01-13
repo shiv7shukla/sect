c0619dfc7e3c720d43603f8ae6d64a571c8e4573
Shiv Shukla <shivshukla00514@gmail.com>
2026-01-13 15:58:25 +0530

    removed dist folder

--- backend/dist/config/env.d.ts
+++
@@ -1,6 +0,0 @@
-export declare const ENV: {
-    DB_URL: string;
-    PORT: number;
-    NODE_ENV: "development" | "test" | "production";
-};
-//# sourceMappingURL=env.d.ts.map
--- backend/dist/config/env.js
+++
@@ -1,9 +0,0 @@
-import { z } from "zod";
-const EnvSchema = z.object({
-    DB_URL: z.string().url(),
-    // JWT_SECRET: z.string().min(1),
-    PORT: z.coerce.number().int().positive(),
-    NODE_ENV: z.enum(["development", "test", "production"]).default("development"),
-});
-export const ENV = EnvSchema.parse(process.env);
-//# sourceMappingURL=env.js.map
