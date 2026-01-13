1e335e8df4d3d456764f04cdb671406d1f7f0e48
Shiv Shukla <shivshukla00514@gmail.com>
2026-01-13 21:35:20 +0530

    coderabbit's suggestions

--- backend/src/middlewares/protectRoute.middleware.ts
+++ backend/src/middlewares/protectRoute.middleware.ts
@@ -6,10 +6,16 @@ import { User } from "../models/userModel.js";
 export const protectRoute=async(req:Request, res:Response, next:NextFunction)=>{
   const token=req.cookies.jwt;
   if(!token) return res.status(401).json({"msg":"Unauthorized - token not provided"});
-  const decode=jwt.verify(token, ENV.JWT_SECRET) as { id: string };
-  if(!decode) return res.status(401).json({"msg":"Unauthorized - invalid token"});
-  const user=await User.findById(decode.id);
-  if (!user) return res.status(401).json({ msg: "Invalid token" });
-  req.user=user;
-  next();
+  try{
+    const decode = jwt.verify(token, ENV.JWT_SECRET) as { id: string };
+    if(!decode) return res.status(401).json({"msg":"Unauthorized - invalid token"});
+    const user=await User.findById(decode.id);
+    if (!user) return res.status(401).json({ msg: "Invalid token" });
+    req.user=user;
+    next();
+  } 
+  catch (error){
+    if (error instanceof jwt.JsonWebTokenError) return res.status(401).json({ msg: "Unauthorized - invalid token" });
+    return res.status(500).json({ msg: "Internal server error" });
+  }
 }
