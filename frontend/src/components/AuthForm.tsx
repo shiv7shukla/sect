ffa6ed541c48429baa5db768e270c05a3f8b1dc7
Shiv Shukla <shivshukla00514@gmail.com>
2026-01-20 00:22:59 +0530

    schema changes

--- frontend/src/components/AuthForm.tsx
+++ frontend/src/components/AuthForm.tsx
@@ -4,10 +4,10 @@ import {z} from "zod"
 const AuthForm = () => {
 
   const signUpSchema=z.object({
-    email:z.string().trim().nonempty("Email is required").email({message:"Email is invalid"}).,
-    password:z.string().trim().nonempty("Password is required").min(6, {message:"Password must be astleast 6 charcters"}).max(12, {message:"Password cannot exceed 12 charcters"}).,
-    username:z.string().trim().min(6, {message:"Username must be astleast 6 charcters"}).max(12, {message:"Username cannot exceed 12 charcters"}).regex(/^[a-zA-Z0-9_]+$/,{message:"Username can only have letters, numbers and underscores"})
-  })
+  email: z.string().trim().min(1, "Email is required").email({ message: "Email is invalid" }),
+  password: z.string().trim().min(1, "Password is required").min(6, { message: "Password must be at least 6 characters" }).max(12, { message: "Password cannot exceed 12 characters" }),
+  username: z.string().trim().min(6, { message: "Username must be at least 6 characters" }).max(12, { message: "Username cannot exceed 12 characters" }).regex(/^[a-zA-Z0-9_]+$/, { message: "Username can only have letters, numbers and underscores" })
+});
 
   return (
     <>
