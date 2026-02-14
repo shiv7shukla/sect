f112dd5c50686a18d50eae91adf8c38a673ba9bc
Shiv Shukla <shivshukla00514@gmail.com>
2026-02-14 23:23:08 +0530

    add: conversation list created for mapping conversations

--- frontend/src/components/ConversationList.tsx
+++ frontend/src/components/ConversationList.tsx
@@ -5,13 +5,13 @@ type ConversationListProps = {
   username: string;
   lastMessagePreview?: string;
   lastMessageAt?: string;
-  seelcteduser: string;
+  selectedUser: string;
 }
 
-const ConversationList = ({username, lastMessagePreview, lastMessageAt, selecteduser}: ConversationListProps) => {
+const ConversationList = ({username, lastMessagePreview, lastMessageAt, selectedUser}: ConversationListProps) => {
   return (
     <>
-      <div className={`h-16 w-[20vw] flex items-center justify-evenly gap-2 rounded-xl mb-2 bg-l-transparent transition-colors duration-200 hover:bg-[#171A21] px-2 hover:cursor-pointer {selectedUser !== null ? "border-l-2 border-l-emerald-400 "}`}>
+      <div className={`h-16 w-[20vw] flex items-center justify-evenly gap-2 rounded-xl mb-2 bg-transparent transition-colors duration-200 hover:bg-[#171A21] px-2 hover:cursor-pointer ${selectedUser !== null ? "border-l-2 border-l-emerald-400" : "border-l-transparent"}`}>
         <div className='h-12 w-12 rounded-full bg-[#171A21] relative'><User className='text-white absolute right-3 top-3' size={24} /></div>
         <div className='h-12 w-[83%] bg-transparent flex flex-col'>
           <div className='w-full text-white text-base'>Shiv Shukla</div>
