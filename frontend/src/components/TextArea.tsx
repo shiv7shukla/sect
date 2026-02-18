3344d38afac23ea7be97bb1b79b72970f56d0d4e
Shiv Shukla <shivshukla00514@gmail.com>
2026-02-18 19:07:51 +0530

    fix: implement changes suggested by coderabbit

--- frontend/src/components/TextArea.tsx
+++ frontend/src/components/TextArea.tsx
@@ -1,9 +1,11 @@
-import { Lock, SendHorizonal } from 'lucide-react'
-import React from 'react'
+import { SendHorizontal } from 'lucide-react'
+import React, { useId } from 'react'
 
 const TextArea = () => {
   const [text, setText] = React.useState("");
   const isTyping = text.length > 0;
+  const id = useId();
+  const gradientId = `gradientId-${id}`
 
   return (
     <div className='h-28 w-full flex flex-col items-center justify-between border-t-2 border-t-zinc-800 bg-[#111318] py-4 px-4'>
@@ -14,8 +16,8 @@ const TextArea = () => {
         onChange={(e)=>setText(e.target.value)}
         placeholder='Type a secure message...' 
         className='h-10 w-full rounded-xl border-2 border-zinc-900 focus:border-emerald-400 transition-colors bg-[#171A21] focus:outline-none px-2 placeholder:text-gray-500 text-white' />
-        <button className={`h-10 w-10 bg-emerald-400 rounded-xl ${isTyping? "opacity-100 cursor-pointer ": "opacity-50 cursor-default"} transition-all duration-300`}>
-          <SendHorizonal className={`size-6 text-black translate-x-2 ${isTyping? "-rotate-90": "rotate-0" } transition-transform duration-300`}/>
+        <button className={`h-10 w-10 bg-emerald-400 rounded-xl ${isTyping? "opacity-100 cursor-pointer ": "opacity-50 cursor-default"} transition-all duration-300`} disabled={!isTyping}>
+          <SendHorizontal className={`size-6 text-black translate-x-2 ${isTyping? "-rotate-90": "rotate-0" } transition-transform duration-300`}/>
         </button>
       </div>
       <footer className='flex gap-2'>
@@ -27,7 +29,7 @@ const TextArea = () => {
           viewBox="0 0 24 24"
           className="lucide-lock">
           <defs>
-            <linearGradient id="lockGradient" x1="0%" y1="0%" x2="0%" y2="100%">
+            <linearGradient id={gradientId} x1="0%" y1="0%" x2="0%" y2="100%">
               <stop offset="0%" stopColor="#e5e7eb" />   {/* light top */}
               <stop offset="45%" stopColor="#000000" /> {/* dark middle */}
               <stop offset="100%" stopColor="#f97316" />{/* orange bottom */}
