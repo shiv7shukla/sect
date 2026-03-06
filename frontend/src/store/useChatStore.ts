d9b3308162a99c14837d70267f32c4df57aa111f
Shiv Shukla <shivshukla00514@gmail.com>
2026-03-06 14:48:09 +0530

    feat: installed ws packages, built ws server on top of existing server

--- frontend/src/store/useChatStore.ts
+++ frontend/src/store/useChatStore.ts
@@ -8,7 +8,7 @@ export type getMessageAPIResponse = {
     conversationId: string;
     type: string;
     lastMessagePreview: string;
-    lastMessageAt: string;
+    lastMessageAt: Date;
   },
   messageInfo: Message[]
 }
@@ -20,7 +20,7 @@ export type Message = {
   content: {
     text: string;
   };
-  createdAt: string;
+  createdAt: Date;
 }
 
 export type Conversations = {
