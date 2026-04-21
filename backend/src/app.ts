3e3142709848f828f393cee679fbf1ecbd973a12
Shiv Shukla <shivshukla00514@gmail.com>
2026-04-21 06:36:30 +0530

    deployment

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -9,6 +9,7 @@ import { app } from "./lib/socket.js"
 app.use(cors({ origin: process.env.NODE_ENV === "production"? ["https://sect-chat.netlify.app"] : ["http://localhost:5173"], credentials:true }));
 app.use(cookieParser());
 app.use(express.json());
+app.options("*", cors())
 app.use("/api/auth", authRouter);
 app.use("/api/conversations", conversationRouter);
 app.use(errorHandler);
