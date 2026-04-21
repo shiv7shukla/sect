757937b2bb01efe6b5d55983019e5c1007b14ab9
Shiv Shukla <shivshukla00514@gmail.com>
2026-04-21 06:16:36 +0530

    deployment

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -1,6 +1,5 @@
 import axios from "axios";
 
-const baseURL = window.location.href.includes('localhost')
-    ? import.meta.env.VITE_API_BASE_URL : 'https://wallpapers-by-alex.vercel.app';
+const baseURL = window.location.href.includes('localhost')? "http://localhost:3000/api" : import.meta.env.VITE_API_BASE_URL;
 
 export const axiosInstance=axios.create({baseURL, withCredentials: true,})
--- frontend/src/lib/socket.ts
+++ frontend/src/lib/socket.ts
@@ -1,4 +1,4 @@
 import { io } from "socket.io-client";
 
-const socketBaseUrl = "http://localhost:3000";
+const socketBaseUrl = window.location.href.includes('localhost')? "http://localhost:3000/api" : import.meta.env.VITE_API_BASE_URL;
 export const socket = io(socketBaseUrl, {autoConnect: false, withCredentials: true});
